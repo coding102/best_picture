81716724b60c99b8275ce97f809c879ce2fdda7f
Marck <mercercountynj@gmail.com>
2016-02-01 13:46:25 -0500

    photo table

---
+++ db/migrate/20160201184221_create_photos.rb
@@ -0,0 +1,12 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+      t.integer :user_id
+      t.string :image
+      t.text :body
+      t.integer :vote
+
+      t.timestamps null: false
+    end
+  end
+end
