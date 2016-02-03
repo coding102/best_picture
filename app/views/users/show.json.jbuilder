4694580ff449f4ea3734c7fdf755eb4656d1b8ec
Marck <mercercountynj@gmail.com>
2016-02-02 20:21:32 -0500

    user modelcontroller

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :username, :total_likes, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :users
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+    root 'home#index'
+    
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+    
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+    
+    
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
