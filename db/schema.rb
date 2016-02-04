56d759c8fc8d13f3df034c6fada59d067af7c885
Marck <mercercountynj@gmail.com>
2016-02-04 11:21:12 -0500

    bug fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203010533) do
+ActiveRecord::Schema.define(version: 20160203190942) do
 
   create_table "photos", force: :cascade do |t|
     t.integer  "user_id"
