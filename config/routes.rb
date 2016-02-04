212be0577ffc9ff8ced614f19b787e59edf5362d
Marck <mercercountynj@gmail.com>
2016-02-04 14:33:21 -0500

    fixed index page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'pages#show', page: 'home'
-  root 'users#index'
+  root 'pages#show', page: "home"    
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
