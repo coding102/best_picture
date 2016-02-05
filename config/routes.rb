7b0e32201438c21c12d684b6c8d04b0acdc79574
Marck <mercercountynj@gmail.com>
2016-02-05 10:32:04 -0500

    fixed routing in controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     
   get 'signup' => 'users#new'
 
-  resources :users, :photos
+    resources :users, :photos, :sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
