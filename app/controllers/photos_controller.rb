521ebe262fbc7079f77b620da0374460e7897ac4
Marck <mercercountynj@gmail.com>
2016-02-24 21:15:22 -0500

    fixed photo upload routing

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -29,7 +29,7 @@ class PhotosController < ApplicationController
     @photo = Photo.create(photo_params)
     respond_to do |format|
       if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+        format.html { redirect_to photos_path, notice: 'Photo was successfully created.' }
         format.json { render :show, status: :created, location: @photo }
       else
         format.html { render :new }
