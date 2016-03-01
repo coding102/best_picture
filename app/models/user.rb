66a2b324f02a8804334dc9e8ec6b833995d39778
Jon Wexler <jwexman@gmail.com>
2016-03-01 12:24:58 -0500

    user deleting photo, modal for adding photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
     has_many :photos
-	       has_secure_password 
-	       validates_confirmation_of :password
-	       validates_presence_of :password, on: :create
+    has_secure_password 
+    validates_confirmation_of :password
+    validates_presence_of :password, on: :create
+
+
+    # scope :photo_today, -> { where("createdAt BETWEEN ? AND ?", Time.now - (60 * 60 * 24), Time.now) }
 
 end
+
