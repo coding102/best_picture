3e34d0bc89329aabce3e97c224e842f9278ddf3f
Marck <mercercountynj@gmail.com>
2016-03-01 15:41:29 -0500

    scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
     validates_confirmation_of :password
     validates_presence_of :password, on: :create
 
-
+    # scope :photo_today, lambda{ where(:created_at => (Time.now.beginning_of_day..Time.now.end_of_day)) }
     # scope :photo_today, -> { where("createdAt BETWEEN ? AND ?", Time.now - (60 * 60 * 24), Time.now) }
 
 end
