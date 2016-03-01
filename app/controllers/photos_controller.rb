26a21de8ec6bf6efe56e9bfbcb58d882a49b46d6
Marck <mercercountynj@gmail.com>
2016-03-01 09:56:22 -0500

    test

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+    before_action :set_photo, only: [:show, :edit, :update, :destroy]
     before_action :correct_user, only: :destroy
     
     
@@ -87,4 +87,3 @@ class PhotosController < ApplicationController
       params.require(:photo).permit(:user_id, :avatar, :body, :vote)
     end
 end
-
